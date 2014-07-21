702fbae1df0879d0763251fa2ef0581273ef8d4b
Lindsay Simpkins <LWSimpkins@users.noreply.github.com>
2014-07-20 22:12:14 -0400

    Added custom toast
    Created a custom toast with an image and text

--- app/src/main/java/com/example/lindsay/toasttest/MainActivity.java
+++ app/src/main/java/com/example/lindsay/toasttest/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.lindsay.toasttest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 
@@ -23,13 +25,29 @@ public class MainActivity extends Activity {
  */
 
 public void showToast(View view){
-    Toast toast = Toast.makeText(this, "Hello from APP", Toast.LENGTH_SHORT);
-    toast.show();
+    Toast toast;
+    if(view.getId()==R.id.button1){
+        toast = Toast.makeText(this, "Hello from APP", Toast.LENGTH_SHORT);
+        toast.show();
+        /*
+        * Control location of the toast using setGravity(gravity, x, y)
+        * Can specify multiple gravities, i.e. Gravity.BOTTOM|Gravity.RIGHT to move toast to bottom right
+        */
+         toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
+    }
+    if(view.getId()==R.id.button2){
+        toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
 
-    /*
-     * Control location of the toast using setGravity(gravity, x, y)
-     * Can specify multiple gravities, i.e. Gravity.BOTTOM|Gravity.RIGHT to move toast to bottom right
-     */
-    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
+        //To use the custom toast_layout XML file
+        LayoutInflater inflater = getLayoutInflater();
+        //Gets the layout file, and sets the root of the XML file (in this case the LinearLayout)
+        //Returns a View object that represents the appearance of the toast
+        View appearance = inflater.inflate(R.layout.toast_layout, (ViewGroup)findViewById(R.id.root));
+        toast.setView(appearance);
+        toast.show();
+    }
+
 }
 }
